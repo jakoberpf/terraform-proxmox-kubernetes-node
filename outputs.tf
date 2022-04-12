b962fdcf2d2c3ebe5a04944b013267b88eb18a63
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-12 23:38:43 +0200

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "local_ip" {
+output "name" {
+  value = "${var.name}-${random_string.deployment_id.result}"
+}
+
+output "ip" {
   value = var.network_ip
 }
 
-output "zt_ip" {
+output "ip_zerotier" {
   value = var.zerotier_ip_assignment
 }
